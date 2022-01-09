11ab6a17c6e9f561900d65b5af63654b047bd71e
rameshMonika <Monika.20@ichat.sp.edu.sg>
2022-01-09 23:15:41 +0800

    udpate

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,11 +2,21 @@ import { useLoaderData } from "remix";
 import { db } from "~/utils/db.server";
 
 export async function loader(){
-  const data={
-    toDo: await db.toDo.findMany()
-  }
-  
-  return data
+
+
+  const data = {
+    toDos: await db.todo.findUnique({
+      where: {
+        todo_id: 1,
+      },
+      select: {
+        todo_id: true,
+        description: true
+      }
+    })
+  };
+  // db.$disconnect();
+  return data;
 }
 
 export default function Index() {
@@ -14,11 +24,24 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>quizment</h1> 
+      <p>Name: {data.toDos.todo_id}</p>
+        <p>Uid: {data.toDos.description}</p>
+
+{/* 
+      {datas.map((data) => (
+          <li key={data.todo_id}>
+          
+              <h3>{data.todo_id}</h3>
+              <h3>{data.description}</h3>
+             
+          </li>
+        ))}
+    
   {data.map((dataT)=>(
     <li key={dataT.todo_id}>
       <h1>{dataT.description}</h1>
     </li>
-  ))}
+  ))} */}
    
      
     </div>
